e05fb6cc15761f7ba00216e9e9d7f3f7ecfe5312
Anish Kumar <62415768+AnishKumar1253@users.noreply.github.com>
2022-12-31 14:36:00 +0530

    Update UserController.java

--- EvaluationProblem2/src/main/java/com/masai/app/controllers/UserController.java
+++ EvaluationProblem2/src/main/java/com/masai/app/controllers/UserController.java
@@ -25,46 +25,40 @@ public class UserController {
 	@Autowired
 	private UserServiceImpl userServiceImpl;
 	
-	@GetMapping("/")
-	public List<User> getAllUsers() {
+	@PostMapping("/users")
+	public ResponseEntity<?> createUser(@RequestBody User user){
+		User u=this.userServiceImpl.createUser(user);
+		return ResponseEntity.ok(u);
+	}
+	
+	@PostMapping("/user/{id}/mail")
+	public ResponseEntity<?> createEmail(@RequestBody Email email){
+		Email e=this.userServiceImpl.createEmail(email);
+		return ResponseEntity.ok(e);
+	}
+
+	@GetMapping("/users")
+	public ResponseEntity<?> getAllUsers(){
 		List<User> users=this.userServiceImpl.getAllUsers();
-		return users;
+		return ResponseEntity.ok(users);
 	}
 	
-	@PostMapping("/")
-	public ResponseEntity<?> createUser(@RequestBody User book) {
-		User b=this.userServiceImpl.createUser(book);
-		if(b==null) {
-			return ResponseEntity.ok("The User already exists hence could not be created");
-		}
-		return ResponseEntity.ok(b);
+	@GetMapping("/users/{id}")
+	public ResponseEntity<?> getUserById(@PathVariable("id") int userId){
+		User user=this.userServiceImpl.getOneUser(userId);
+		return ResponseEntity.ok(user);
 	}
 	
-//	@PutMapping("/{bookId}")
-//	public ResponseEntity<?> updateUser(@RequestBody String price,@PathVariable("bookId") int bookId){
-////		System.out.println(price.substring(15,18));
-//		int bookPrice=Integer.parseInt(price.substring(15,18)); 
-//		User book=this.userServiceImpl.getUser(bookId);
-////		 if(book!=null) {
-////			 this.userServiceImpl.updateUserPrice(book, bookPrice);
-////			 return ResponseEntity.ok("the price of the book has been updated with "
-////			 		+ "the given value");
-////		 }
-//		 return ResponseEntity.ok("the book is not present");
-//	}
-	
-	
-	
-	@GetMapping("/{id}")
-	public User getUser(@PathVariable("bookId") int bookId) {
-		User book=this.userServiceImpl.getUser(bookId);
-		return book;
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<?> deleteUser(@PathVariable("id") int userId){
+		this.userServiceImpl.deleteUser(userId);
+		return ResponseEntity.ok("User has been successfully deleted");
 	}
 	
-	@DeleteMapping("/{id}")
-	public String deleteUser(@PathVariable("bookId") int bookId) {
-		this.userServiceImpl.deleteUser(bookId);
-		return "User has been deleted successfully";
+	@GetMapping("/users/{id}/mail/{mail_id}")
+	public ResponseEntity<?> getEmailForUser(@PathVariable("id") int userId){
+		Email email=this.userServiceImpl.getEmail(userId);
+		return ResponseEntity.ok(email);
 	}
 }
 
